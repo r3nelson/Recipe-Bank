b521be180a54a74781c153bb50b8a3127fe02755
Richard Nelson <r3nelson@outlook.com>
2025-02-04 00:40:33 -1000

    added new columns to recipes table. Added constraint for rating

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, ARRAY
+from sqlalchemy import Column, Integer, String, Float, Boolean, ARRAY, CheckConstraint
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -7,9 +8,15 @@ class Recipe(Base):
     __tablename__ = "recipes"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
+    name = Column(String, nullable=False)
     haveCooked = Column(Boolean, default=False)
-    ingredients = Column(ARRAY(String), nullable=False)
-    instructions = Column(ARRAY(String), nullable=False)
+    ingredients = Column(JSONB, nullable=False) 
+    directions = Column(ARRAY(String), nullable=False)
+    quantity = Column(Float, nullable=True)
+    prepTime = Column(Integer, nullable=True)
     cookTime = Column(Integer, nullable=True)
     rating = Column(Float, nullable=True) 
+
+    __table_args__ = (
+        CheckConstraint("rating >= 0 AND rating <= 5", name="rating_range"),
+    )
