3fadc7e361ce2e6aa8dc452889837591e1ed1287
Richard Nelson <r3nelson@outlook.com>
2025-02-07 10:39:50 -1000

    updated App with navbar and RecipeCard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+// import RecipesList from "./components/RecipesList";
 import RecipeCard from "./components/RecipeCard";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <RecipeCard></RecipeCard>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <RecipeCard recipe_id={3}></RecipeCard>
+      {/* <RecipesList></RecipesList>; */}
+    </div>
+  );
 }
 
 export default App;
