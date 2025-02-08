d30a0b4d7385ba33f0ec42efde81b703c397fedb
Richard Nelson <r3nelson@outlook.com>
2025-02-08 00:42:24 -1000

    updated App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,12 @@
-// import RecipesList from "./components/RecipesList";
 import RecipeCard from "./components/RecipeCard";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar></Navbar>
       <RecipeCard recipe_id={3}></RecipeCard>
-      {/* <RecipesList></RecipesList>; */}
-    </div>
+    </>
   );
 }
 
