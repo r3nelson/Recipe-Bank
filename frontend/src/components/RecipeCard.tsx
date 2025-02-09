581f3448c046e976c3ae18e8fdef96c4a3c0c75a
Richard Nelson <r3nelson@outlook.com>
2025-02-09 01:14:12 -1000

    cleaned up RecipeCard by seperating logic into smaller components

--- frontend/src/components/RecipeCard.tsx
+++ frontend/src/components/RecipeCard.tsx
@@ -1,105 +1,54 @@
-import { Recipe } from "../types/recipe";
-import { fetchRecipe } from "../api/recipeAPI";
-import { useEffect, useState } from "react";
+import useGetRecipeByID from "../hooks/useGetRecipeByID";
+import Directions from "./Directions";
+import Ingredients from "./Ingredients";
+import SectionHeader from "./SectionHeader";
+import HaveCookedBox from "./HaveCookedBox";
+import GeneralRecipeInfo from "./GeneralRecipeInfo";
 
 type RecipeCardProps = {
   recipe_id: number;
 };
 
 export default function RecipeCard({ recipe_id }: RecipeCardProps) {
-  const [recipe, setRecipe] = useState<Recipe | null>(null);
+  const { recipe, isLoading, error } = useGetRecipeByID(recipe_id);
 
-  useEffect(() => {
-    async function loadRecipe() {
-      const data = await fetchRecipe(recipe_id);
-      setRecipe(data);
-    }
-    loadRecipe();
-  }, [recipe_id]);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-  return (
-    <div className="relative p-10 border text-center  m-3 w-3xl">
-      {recipe ? (
-        // haveCooked container
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (recipe) {
+    return (
+      <div className="relative p-10 border text-center  m-3 w-3xl">
         <div>
-          <div className="absolute top-3 right-3 flex items-center space-x-2">
-            <label htmlFor="cooked" className="text-sm font-semibold">
-              Cooked?
-            </label>
-            <input
-              type="checkbox"
-              id="cooked"
-              checked={recipe.haveCooked}
-              readOnly
-              className="w-4 h-4 accent-green-600 cursor-pointer"
-            />
-          </div>
+          <HaveCookedBox haveCooked={recipe.haveCooked}></HaveCookedBox>
+
           {/* General Info */}
           <div className="m-5">
-            <h2 className="text-xl font-bold">{recipe.name}</h2>
-            <p>
-              Quantity: {recipe.quantity} | Prep Time: {recipe.prepTime} min |
-              Cook Time: {recipe.cookTime} min
-            </p>
+            <GeneralRecipeInfo
+              name={recipe.name}
+              quantity={recipe.quantity}
+              prepTime={recipe.prepTime}
+              cookTime={recipe.cookTime}
+            ></GeneralRecipeInfo>
           </div>
 
           <div>
-            {/* Ingredients */}
             <div className="m-5">
-              <div className="flex items-center my-4">
-                <div className="flex-grow border-t border-black"></div>
-                <span className="px-4 text-black font-semibold">
-                  Ingredients
-                </span>
-                <div className="flex-grow border-t border-black"></div>
-              </div>
-
-              <ul
-                className={
-                  recipe.ingredients.length <= 15
-                    ? "grid grid-cols-3 "
-                    : "grid grid-cols-4"
-                }
-              >
-                {recipe.ingredients.map((ing, index) => (
-                  <li
-                    className={
-                      recipe.ingredients.length < 3
-                        ? ""
-                        : recipe.ingredients.length > 15 &&
-                          (index + 1) % 4 !== 0
-                        ? " border-r border-black mr-1 pr-1 pl-1"
-                        : (index + 1) % 3 !== 0
-                        ? " border-r border-black mr-1 pr-1 pl-1"
-                        : ""
-                    }
-                    key={index}
-                  >
-                    {`${ing.quantity} ${ing.measurement_type} ${ing.name}`}
-                  </li>
-                ))}
-              </ul>
+              <SectionHeader header="Ingredients"></SectionHeader>
+              <Ingredients ingredients={recipe.ingredients}></Ingredients>
             </div>
           </div>
 
-          {/* Directions */}
           <div className="m-5">
-            {/* <h3>Directions</h3> */}
-            <div className="flex items-center my-4">
-              <div className=" flex-grow border-t border-black"></div>
-              <span className="px-4 text-black font-semibold">Directions</span>
-              <div className="flex-grow border-t border-black"></div>
-            </div>
-            <ol className="list-decimal text-left">
-              {recipe.directions.map((direction, index) => (
-                <li key={index}>{direction}</li>
-              ))}
-            </ol>
+            <SectionHeader header="Directions"></SectionHeader>
+            <Directions directions={recipe.directions}></Directions>
           </div>
         </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  );
+      </div>
+    );
+  }
 }
