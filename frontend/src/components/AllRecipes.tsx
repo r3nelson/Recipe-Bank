35e2f29dda71976d0ff43918d05bbd5ce1ab5cc0
Richard Nelson <r3nelson@outlook.com>
2025-03-14 01:37:12 -1000

    updated searchbar to be a part of all recipes. Conditionally render results in allrecipes

--- frontend/src/components/AllRecipes.tsx
+++ frontend/src/components/AllRecipes.tsx
@@ -1,4 +1,7 @@
 import { Recipe } from "../types/recipe";
+import SectionHeader from "./SectionHeader";
+import SearchBar from "./SearchBar";
+import { useState } from "react";
 
 type AllRecipesProps = {
   recipes: Recipe[];
@@ -13,27 +16,45 @@ export default function AllRecipes({
   updateRecipeId,
   onSetShowRecipeOptions,
 }: AllRecipesProps) {
+  const [hideRecipes, setHideRecipes] = useState(false);
   const recipeNamesAndIds: [string, number][] = [];
+
   recipes.map((recipe) =>
     recipeNamesAndIds.push([recipe.name.toLowerCase(), recipe.id])
   );
-
   recipeNamesAndIds.sort();
 
   return (
     <div
-      className="flex flex-col justify-center items-center text-xl"
+      className="flex flex-col justify-center items-center text-lg"
       onClick={onSetShowRecipeOptions}
     >
-      All Recipes
-      {showRecipeOptions && (
-        <ul className="text-base border">
-          {recipeNamesAndIds.map((recipe, index) => (
-            <li key={index} onClick={() => updateRecipeId(recipe[1])}>
-              {recipe[0]}
-            </li>
-          ))}
-        </ul>
+      {showRecipeOptions ? (
+        <div className="relative p-10 border text-center m-3 w-xl">
+          <SearchBar
+            recipes={recipes}
+            onHideRecipesChange={setHideRecipes}
+          ></SearchBar>
+
+          {!hideRecipes && (
+            <>
+              <SectionHeader header="All Recipes"></SectionHeader>
+              <ul className="text-md grid grid-cols-3">
+                {recipeNamesAndIds.map((recipe, index) => (
+                  <li
+                    className="cursor-pointer hover:bg-gray-200"
+                    key={index}
+                    onClick={() => updateRecipeId(recipe[1])}
+                  >
+                    {recipe[0]}
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
+        </div>
+      ) : (
+        "All Recipes"
       )}
     </div>
   );
--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,39 +1,53 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Recipe } from "../types/recipe";
+import SectionHeader from "./SectionHeader";
 
 type SearchBarProps = {
   recipes: Recipe[];
+  onHideRecipesChange: (hide: boolean) => void;
 };
 
-export default function SearchBar({ recipes }: SearchBarProps) {
-  // this is where I'll return a tri
-  console.log(recipes);
+export default function SearchBar({
+  recipes,
+  onHideRecipesChange,
+}: SearchBarProps) {
+  // Maybe use a tri
   const [searchQuery, setSearchQuery] = useState("");
+  const [hideRecipes, setHideRecipes] = useState(false);
 
   const filteredRecipes = recipes.filter((recipe) =>
     recipe.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
-  console.log(
-    `filteredRecipes: ${filteredRecipes.map((recipe) => recipe.name)}`
-  );
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+    setSearchQuery(value);
+    setHideRecipes(value !== "");
+  }
+
+  useEffect(() => {
+    onHideRecipesChange(hideRecipes);
+  }, [hideRecipes]);
 
   return (
     // search bar
-    <div>
+    <div className="border">
       <input
         type="text"
         placeholder="Search recipes"
         value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        className="border m-2 p-1 rounded w-1/4"
+        onChange={handleChange}
+        className="border m-2 p-1 rounded w-1/2"
       ></input>
-      {filteredRecipes.length > 0 && (
-        <ul>
-          {filteredRecipes.map((recipe, index) => (
-            <li key={index}>{recipe.name}</li>
-          ))}
-        </ul>
+      {hideRecipes && <SectionHeader header="All Recipes"></SectionHeader>}
+      {searchQuery && filteredRecipes.length > 0 && (
+        <div className=" p-10  m-3 w-full">
+          <ul className="text-md grid grid-cols-3 gap-1">
+            {filteredRecipes.map((recipe, index) => (
+              <li key={index}>{recipe.name}</li>
+            ))}
+          </ul>
+        </div>
       )}
     </div>
   );
