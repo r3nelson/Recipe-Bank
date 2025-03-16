73fe9afb9e4ad4840f676e5914228540040c2fa9
Richard Nelson <r3nelson@outlook.com>
2025-03-16 02:12:24 -1000

    created form for updating recipes

--- frontend/src/api/recipeAPI.ts
+++ frontend/src/api/recipeAPI.ts
@@ -30,6 +30,20 @@ export async function fetchRecipe(recipe_id: number): Promise<Recipe | null> {
   }
 }
 
+export async function getIds(): Promise<number[]> {
+  const response = await fetch(`${baseURL}-ids`);
+  try {
+    if (!response.ok) {
+      throw new Error(`Failed to fetch ids: ${response.statusText}`);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching ids:", error);
+    return [];
+  }
+}
+
 export async function createRecipe(recipe: Recipe) {
   try {
     const response = await fetch(baseURL, {
@@ -46,6 +60,27 @@ export async function createRecipe(recipe: Recipe) {
     }
 
     console.log("Recipe added successfully!");
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+export async function updateRecipe(recipe_id: number, recipe: Recipe) {
+  try {
+    const response = await fetch(`${baseURL}/${recipe_id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(recipe),
+    });
+    console.log(JSON.stringify(recipe));
+
+    if (!response.ok) {
+      throw new Error("Failed to add recipe");
+    }
+
+    console.log(`Recipe ${recipe.name} updated successfully!`);
   } catch (error) {
     console.error("Error:", error);
   }
